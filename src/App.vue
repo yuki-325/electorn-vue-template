60eac2273f7748e80adc3b53f000e1f9edfff811
中野勇貴 <sakeikura325325@gmail.com>
2024-06-30 19:51:03 +0900

    [fix] uiコンポーネントを追加

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <h1>Hello Electron with Vue and Webpack!</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -11,12 +15,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+nav {
+  display: flex;
+  gap: 10px;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import About from '../views/About.vue';
+import Home from '../views/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
